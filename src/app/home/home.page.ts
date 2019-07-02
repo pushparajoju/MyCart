be5e46748d899f85930c9b98a5a8e4831b8fd05c
Pushpa Rajoju <purajoju@in.ibm.com>
2019-07-02 16:56:52 +0530

    Cart Application

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+  cart = [];
+  items = [];
+
+  sliderConfig = {
+    slidesPerView: 1.6,
+    spaceBetween: 10,
+    centeredSlides: true
+  };
+
+  constructor(public router: Router, public cartService: CartService) { }
+
+  ngOnInit() {
+    this.items = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+  }
+
+  addToCart(product) {this.cartService.addProduct(product); }
+  openCart() { this.router.navigate(['cart']); }
 
 }
